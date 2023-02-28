3791f2dcfeb7c4a686094bf50055eee2dbf05cb8
jzbor <jzbor@posteo.net>
2023-02-28 01:45:41 +0100

    Adding desktop-gnome.nix

---
+++ desktop-gnome.nix
@@ -0,0 +1,8 @@
+{ lib, config, pkgs, ... }:
+
+{
+  # Gnome
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+}
