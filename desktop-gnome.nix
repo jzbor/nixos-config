92eef0d866e5f327260c3e6d5a02a0ceb3c0a84a
jzbor <jzbor@posteo.net>
2023-02-28 23:29:25 +0100

    Adding dash-to-dock for gnome

--- desktop-gnome.nix
+++ desktop-gnome.nix
@@ -27,6 +27,7 @@
   environment.systemPackages = (with pkgs.gnomeExtensions; [
     appindicator
     blur-my-shell
+    dash-to-dock
   ]) ++ (with pkgs.gnome; [
     gnome-tweaks
   ]) ++ (with pkgs; [
