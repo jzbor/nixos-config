2e5865c73e43fe59de6c47ca17f0765f28bc1664
jzbor <jzbor@posteo.net>
2023-02-28 21:31:17 +0100

    Adding machine specific configs

---
+++ machine/gpu/intel.nix
@@ -0,0 +1,17 @@
+# source: https://github.com/NixOS/nixos-hardware/blob/master/common/gpu/intel/default.nix
+
+{ config, lib, pkgs, ... }:
+
+{
+  boot.initrd.kernelModules = [ "i915" ];
+
+  environment.variables = {
+    VDPAU_DRIVER = lib.mkIf config.hardware.opengl.enable (lib.mkDefault "va_gl");
+  };
+
+  hardware.opengl.extraPackages = with pkgs; [
+    vaapiIntel
+    libvdpau-va-gl
+    intel-media-driver
+  ];
+}
