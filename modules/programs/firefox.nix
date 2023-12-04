5160fb8b8b028f3cdcddfa15985886e6bfdeaa44
jzbor <jzbor@posteo.net>
2023-12-04 12:40:07 +0100

    Enabling further Firefox policies

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -5,6 +5,8 @@
     policies = {
       DisablePocket = true;
       DisableFormHistory = true;
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
       EnableTrackingProtection = true;
       Cookies = {
         AcceptThirdParty = "never";
@@ -17,6 +19,14 @@
         "uBlock0@raymondhill.net" = {
           installation_mode = "force_installed";
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+        };
+        "gdpr@cavi.au.dk" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/consent-o-matic/latest.xpi";
+        };
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
         };
       };
     };
