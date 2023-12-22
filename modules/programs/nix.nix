2309ae489669a9ac8d26f1668fa71fddad29231d
jzbor <jzbor@posteo.net>
2023-12-22 20:24:55 +0100

    Use overlay as exported nixpkgs

--- modules/programs/nix.nix
+++ modules/programs/nix.nix
@@ -30,6 +30,6 @@
 
   # Make nixpkgs available to local nix commands like `nix shell` or `nix-shell`
   nix.nixPath = [ "nixpkgs=/etc/channels/nixpkgs" "nixos-config=/etc/nixos/configuration.nix" "/nix/var/nix/profiles/per-user/root/channels" ];
-  environment.etc."channels/nixpkgs".source = inputs.nixpkgs.outPath;
-  nix.registry.nixpkgs.flake = inputs.nixpkgs;
+  environment.etc."channels/nixpkgs".source = inputs.jzbor-overlay.outPath;
+  nix.registry.nixpkgs.flake = inputs.jzbor-overlay;
 }
