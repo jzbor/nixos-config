b24c9f6aafd1b3ccf0dc835c2f4dcf74208ddb3d
jzbor <jzbor@posteo.net>
2024-01-14 01:10:07 +0100

    Adding apps for rebuilding and maintenance

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    cf.url = "github:jzbor/cornflakes";
+    cf.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     jzbor-overlay.inputs.nixpkgs.follows = "nixpkgs";
@@ -14,7 +16,7 @@
   };
 
 
-  outputs = { nixpkgs, home-manager, jzbor-overlay, nixos-hardware, nix-colors, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, cf, jzbor-overlay, nixos-hardware, nix-colors, ... }@inputs:
   let
     pkgs-x86_64 = (import nixpkgs {
       system = "x86_64-linux";
@@ -119,5 +121,77 @@
         ];
         extraSpecialArgs = { inherit nix-colors; };
       });
-  };
+  } // cf.lib.flakeForDefaultSystems (system:
+  let
+    pkgs = nixpkgs.legacyPackages."${system}";
+  in {
+
+    ### APPS ###
+    apps.rebuild = cf.lib.createShellApp system {
+      name = "rebuild";
+      text = ''
+      if [ "$UID" = 0 ]; then
+        SUDO=""
+      elif command -v doas >/dev/null; then
+        SUDO="doas"
+      elif command -v sudo >/dev/null; then
+        SUDO="sudo"
+      fi
+
+      set -x
+      $SUDO nixos-rebuild switch --flake "${self}" "$@"
+      '';
+    };
+
+    apps.rebuild-home = cf.lib.createShellApp system {
+      name = "rebuild-home";
+      text = ''
+      if [ "$UID" != 0 ]; then
+        set -x
+        ${home-manager.packages.${system}.default}/bin/home-manager switch --flake "${self}" "$@"
+      else
+        echo "Nothing to be done for user '$USER'"
+      fi
+      '';
+    };
+
+    apps.cleanup = let
+      expireAfterDays = "30";
+    in
+    cf.lib.createShellApp system {
+      name = "cleanup";
+      text = ''
+      if [ "$UID" = 0 ]; then
+        SUDO=""
+      elif command -v doas >/dev/null; then
+        SUDO="doas"
+      elif command -v sudo >/dev/null; then
+        SUDO="sudo"
+      fi
+
+      if [ "$UID" != 0 ]; then
+        set -x
+        ${home-manager.packages.${system}.default}/bin/home-manager expire-generations "${expireAfterDays}days"
+        nix profile wipe-history --older-than "${expireAfterDays}d";
+      else
+        set -x
+      fi
+      $SUDO nix-collect-garbage -d --delete-older-than "${expireAfterDays}d"
+      nix store optimise
+      '';
+    };
+
+    apps.full-maintenance = cf.lib.createShellApp system {
+      name = "full-maintenance";
+      text = ''
+      printf "\n=> Updating system\n"
+      nix run ${self}#rebuild
+      printf "\n=> Updating home\n"
+      nix run ${self}#rebuild-home
+      printf "\n=> Cleaning up afterwards\n"
+      nix run ${self}#cleanup
+      '';
+    };
+
+  });
 }
