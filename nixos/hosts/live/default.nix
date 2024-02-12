1a619cfbd34c11a91def29b36ce596a89dff8835
jzbor <jzbor@posteo.net>
2024-02-12 17:08:46 +0100

    Adding NetworkManager to installer image

--- nixos/hosts/live/default.nix
+++ nixos/hosts/live/default.nix
@@ -1,10 +1,14 @@
-{ pkgs, inputs, ... }:
+{ pkgs, inputs, lib, ... }:
 
 {
   imports = [
         "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix"
         # "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/iso-image.nix"
+
         ../../programs/nix.nix
+        ../../base/security.nix
+        ../../base/networking.nix
+        ../../base/localization.nix
   ];
 
   networking.hostName = "nixos-live";
@@ -14,7 +18,11 @@
     lm_sensors
     stress
     tmux
+    nix-tree
   ];
+
+  networking.wireless.enable = false;  # wpa_supplicant conflicts with NetworkManager
+  networking.networkmanager.plugins = lib.mkForce [];
 
   isoImage.squashfsCompression = "gzip -Xcompression-level 9";
 }
