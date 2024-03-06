108db10ea8fe99b481cdf7353e31a8cb68658f7a
jzbor <jzbor@posteo.net>
2024-03-06 01:39:24 +0100

    Updating fman script

--- home/scripts/fman.sh
+++ home/scripts/fman.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+if [ "$#" -gt 0 ]; then
+	query="$1"
+else
+	query=""
+fi
+
 selection="$(man -k . \
-	| fzf --preview "echo {} | sed 's/\([a-zA-Z0-9_\.-]*\) (\([a-z0-9]*\)).*/man -P cat \2 \1/' | sh" \
+	| fzf --query "$query" --preview "echo {} | sed 's/\([a-zA-Z0-9_\.-]*\) (\([a-z0-9]*\)).*/man -P cat \2 \1/' | sh" \
 	| sed 's/\([a-zA-Z0-9_\.-]*\) (\([a-z0-9]*\)).*/man \2 \1/')"
 if [ -n "$selection" ]; then
 	echo "$selection" | sh
-	fman
+	fman "$(echo "$selection" | cut -f3 -d' ')"
 fi
