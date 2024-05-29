180268fc72d5f92d4eba643d975264d23582c14b
jzbor <jzbor@posteo.net>
2024-05-29 15:07:04 +0200

    Disabling dnssec

--- nixos/base/networking.nix
+++ nixos/base/networking.nix
@@ -11,7 +11,7 @@ with lib;
   networking.nameservers = [ "5.75.234.6#dns.jzbor.de" "9.9.9.9#dns.quad9.net" ];
   services.resolved = {
     enable = true;
-    dnssec = "true";
+    dnssec = "false";
     domains = [ "~." ];
     extraConfig = ''
       DNSOverTLS=yes
