9983f465a23c162a070c34760a594c98e41b2bd8
jzbor <jzbor@posteo.net>
2025-01-22 20:18:16 +0100

    Adding tealdeer and nvim to pinenote home config

--- pinenote/default.nix
+++ pinenote/default.nix
@@ -1,7 +1,17 @@
-_: {
+{ pkgs, ... }: {
+  imports = [
+    ../home/programs/neovim
+  ];
+
+  programs.neovim.enable = true;
+
   home = {
     username = "jzbor";
     homeDirectory = "/home/jzbor";
     stateVersion = "24.11";
+
+    packages = with pkgs; [
+      tealdeer
+    ];
   };
 }
