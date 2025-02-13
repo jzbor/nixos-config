28f3b679204227fc998bcc6d2e97b81642dfb22d
jzbor <jzbor@posteo.net>
2025-02-13 18:51:52 +0100

    Adding pinenote home config

---
+++ hosts/pinenote/users/jzbor/home-configuration.nix
@@ -0,0 +1,19 @@
+{ flake, pkgs, perSystem, ... }:
+
+{
+  imports = [ flake.homeModules.programs ];
+
+  programs.neovim.enable = true;
+
+  home = {
+    username = "jzbor";
+    homeDirectory = "/home/jzbor";
+    stateVersion = "24.11";
+
+    packages = with pkgs; [
+      tealdeer
+      nix-tree
+      perSystem.parcels.nix-sweep
+    ];
+  };
+}
