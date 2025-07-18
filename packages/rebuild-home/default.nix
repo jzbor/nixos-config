b9246805b8f5a52f8b839f6255f7e5e5c105067a
jzbor <jzbor@posteo.net>
2025-07-19 01:40:06 +0200

    Improve custom system and home rebuilding scripts

--- packages/rebuild-home/default.nix
+++ packages/rebuild-home/default.nix
@@ -2,7 +2,6 @@
   name = pname;
   runtimeInputs = with pkgs; [
     attic-client
-    home-manager
     perSystem.self.nix
   ];
   text = let
@@ -11,25 +10,27 @@
     printf "\n=> Rebuilding home\n"
 
     if [ "$UID" != 0 ]; then
+      home_profile=""
+      if [ -d "/nix/var/nix/profiles/per-user/$USER/home-manager" ]; then
+        home_profile="/nix/var/nix/profiles/per-user/$USER/home-manager"
+      elif [ -d "$HOME/.local/state/nix/profiles/home-manager" ]; then
+        home_profile="$HOME/.local/state/nix/profiles/home-manager"
+      else
+        echo "Unable to find home-manager profile path" >/dev/stderr
+        exit 1
+      fi
+
       set -x
-      home-manager switch --flake "${flake}" "$@"
+      path="$(nix build --no-link --print-out-paths "${flake}#homeConfigurations.$USER.activationPackage")"
+      nix-env --profile "$home_profile" --set "$path"
+      "$path/activate"
       set +x
 
       if attic cache info ${cacheName} 2>/dev/null; then
         printf "\n=> Pushing home closure to binary cache (${cacheName})\n"
 
-        homepath=""
-        if [ -d "/nix/var/nix/profiles/per-user/$USER/home-manager" ]; then
-          homepath="/nix/var/nix/profiles/per-user/$USER/home-manager"
-        elif [ -d "$HOME/.local/state/nix/profiles/home-manager" ]; then
-          homepath="$HOME/.local/state/nix/profiles/home-manager"
-        else
-          echo "Unable to find home-manager profile path" >/dev/stderr
-          exit 1
-        fi
-
         set -x
-        attic push ${cacheName} "$homepath" || true
+        attic push ${cacheName} "$path" || true
       fi
     else
       echo "Nothing to be done for user '$USER'"
--- packages/rebuild-system/default.nix
+++ packages/rebuild-system/default.nix
@@ -6,27 +6,31 @@
   ];
   text = let
     cacheName = "desktop";
+    activator = pkgs.writeShellScriptBin "nixos-activator" ''
+      set -x
+      path="$(nix build --no-link --print-out-paths "${flake}#nixosConfigurations.$(< /etc/hostname).config.system.build.toplevel")"
+      nix-env --profile /nix/var/nix/profiles/system --set "$path"
+      "$path/bin/switch-to-configuration" switch
+      set +x
+      echo "$path"
+    '';
   in ''
-        printf "\n=> Rebuilding system\n"
+    printf "\n=> Rebuilding system\n"
 
-        if [ "$UID" = 0 ]; then
-        SUDO=""
-        elif command -v doas >/dev/null; then
-        SUDO="doas"
-        elif command -v sudo >/dev/null; then
-        SUDO="sudo"
-        fi
+    if [ "$UID" = 0 ]; then
+    SUDO=""
+    elif command -v doas >/dev/null; then
+    SUDO="doas"
+    elif command -v sudo >/dev/null; then
+    SUDO="sudo"
+    fi
 
-        set -x
-        path="$(nix build --no-link --print-out-paths ".#nixosConfigurations.$(< /etc/hostname).config.system.build.toplevel")"
-        $SUDO nix-env --profile /nix/var/nix/profiles/system --set "$path"
-        $SUDO "$path/bin/switch-to-configuration" switch
-        set +x
+    path="$($SUDO ${activator}/bin/nixos-activator | tail -n1)"
 
-        if attic cache info ${cacheName} 2>/dev/null; then
-          printf "\n=> Pushing system closure to binary cache (${cacheName})\n"
-          set -x
-          attic push ${cacheName} /run/current-system || true
-        fi
+    if attic cache info ${cacheName} 2>/dev/null; then
+      printf "\n=> Pushing system closure to binary cache (${cacheName})\n"
+      set -x
+      attic push ${cacheName} "$path" || true
+    fi
   '';
 }
