a81af33e5ab998dc179db92acc32a35eaeed1031
jzbor <jzbor@posteo.net>
2025-08-03 14:43:08 +0200

    Update Nix configuration

--- modules/nixos/programs/nix.nix
+++ modules/nixos/programs/nix.nix
@@ -2,48 +2,52 @@
 
 {
   nix = {
-    # Activate nix flakes system wide
     package = perSystem.self.nix;
-    extraOptions = ''
-      min-free = ${toString (16 * 1042 * 1024 * 1024)}
-      max-free = ${toString (32 * 1042 * 1024 * 1024)}
-      http-connections = 128
-      experimental-features = nix-command flakes
-      netrc-file = /etc/nix/netrc
-    '';
 
-    # Keep generated outputs on garbage collection
-    settings.keep-outputs = true;
-    settings.keep-derivations = true;
-    # Auto optimise store on builds
-    settings.auto-optimise-store = true;
     # Automatically run optimiser for nix store
     optimise.automatic = true;
-    # Maximum number of parallel threads in one build job
-    settings.cores = 0; # 0 means all cores
-    # Maximum number of jobs to run in parallel
-    settings.max-jobs = "auto";
-    # Build in sandboxed environment
-    settings.sandbox = true;
-    settings.max-substitution-jobs = 128;
 
-    # Binary caches
-    settings.substituters = [
-      "https://nix-community.cachix.org"
-      "https://cache.nixos.org/"
-      "https://cache.jzbor.de/desktop"
-      "https://cache.jzbor.de/sp"
-      "https://cache.jzbor.de/public"
-    ];
-    settings.trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "desktop:feMjIkyrgSZI+5lrUnKe3iwgGoykffN4ihScaBC/J7w="
-      "sp:v8udot3ROy99u4GTQZ79zsRxpzNLvYrTSN17odOHMnc="
-      "public:AdkE6qSLmWKFX4AptLFl+n+RTPIo1lrBhT2sPgfg5s4="
-    ];
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
 
-    # Disable online registry
-    settings.flake-registry = "";
+      # Garbage Collection
+      keep-outputs = true;
+      keep-derivations = true;
+      auto-optimise-store = true;
+      min-free = (toString (4 * 1042 * 1024 * 1024));
+      max-free = (toString (16 * 1042 * 1024 * 1024));
+
+      # Building
+      cores = 0; # 0 means all cores
+      max-jobs = "auto";
+      sandbox = true;
+
+      # Substitution
+      max-substitution-jobs = 128;
+      http-connections = 128;
+      connect-timeout = 10;
+      fallback = true;
+
+      # Binary caches
+      netrc-file = "/etc/nix/netrc";
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
+        "https://cache.jzbor.de/desktop"
+        "https://cache.jzbor.de/sp"
+        "https://cache.jzbor.de/public"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "desktop:feMjIkyrgSZI+5lrUnKe3iwgGoykffN4ihScaBC/J7w="
+        "sp:v8udot3ROy99u4GTQZ79zsRxpzNLvYrTSN17odOHMnc="
+        "public:AdkE6qSLmWKFX4AptLFl+n+RTPIo1lrBhT2sPgfg5s4="
+      ];
+
+      # Disable online registry
+      flake-registry = "";
+    };
+
   };
 
   # Make nixpkgs available to local nix commands like `nix shell` or `nix-shell`
