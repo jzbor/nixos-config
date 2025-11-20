481b3209a96ba384ca233665eb28cc903f05fb77
jzbor <zborof@posteo.de>
2025-11-20 13:57:54 +0100

    Adding thinkpad_acpi driver to kernel

--- nixosConfigurations/x250/default.nix
+++ nixosConfigurations/x250/default.nix
@@ -13,14 +13,12 @@ in {
     scheme = "traditional";
     secureBoot = true;
   };
-
   boot.lanzaboote.pkiBundle = lib.mkForce "/var/lib/sbctl";
+
+  boot.initrd.availableKernelModules = [ "thinkpad_acpi" ];
 
   # Enable cross building for aarch64
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-
-  # This is automatically enabled by nixos-hardware.nixosModules.lenovo-thinkpad-x1-6th-gen, but should not be needed
-  services.throttled.enable = false;
 
   boot.initrd.luks.devices.crypt0-root.device = "/dev/disk/by-label/crypt0-root";
 
@@ -48,5 +46,4 @@ in {
     "hplip"
   ];
 
-  services.logrotate.checkConfig = false;
 }
