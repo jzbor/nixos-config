62083da2609373b48e16c82d5e4dd8072a0be8f9
jzbor <jzbor@posteo.net>
2025-12-08 21:17:43 +0100

    Fix ollama amd installation

--- nixosConfigurations/desktop-i5/default.nix
+++ nixosConfigurations/desktop-i5/default.nix
@@ -45,18 +45,7 @@ in {
 
   services.ollama = {
     enable = true;
-    # package = pkgs.ollama-rocm;
-    package = let
-      pinPackage = { name, commit, sha256, }: (import (builtins.fetchTarball {
-        inherit sha256;
-        url = "https://github.com/NixOS/nixpkgs/archive/${commit}.tar.gz";
-      }) { inherit (pkgs) system; }).${name};
-    in pinPackage {
-      name = "ollama";
-      commit = "d0169965cf1ce1cd68e50a63eabff7c8b8959743";
-      sha256 = "sha256:1hh0p0p42yqrm69kqlxwzx30m7i7xqw9m8f224i3bm6wsj4dxm05";
-    };
-    acceleration = "rocm";
+    package = pkgs.ollama-rocm;
     rocmOverrideGfx = "10.3.1";
   };
 
