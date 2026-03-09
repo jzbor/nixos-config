09786da27ea5d93fcbd799cea96f1694c9d5bfc9
jzbor <jzbor@posteo.net>
2026-03-10 00:54:56 +0100

    Add menu rule for marswm and allow video group to update backlight

--- nixosModules/base/power-management.nix
+++ nixosModules/base/power-management.nix
@@ -8,5 +8,10 @@ _:
     powertop.enable = true;
     cpuFreqGovernor = "ondemand";
   };
+
+  # Allow updating the backlight
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="intel_backlight", GROUP="video", MODE="0664"
+  '';
 }
 
